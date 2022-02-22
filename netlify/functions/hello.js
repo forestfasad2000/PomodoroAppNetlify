032f47c8877c2ba21f6fa02708685055643cd9d6
forestfasad2000 <forestfasad@gmail.com>
2022-02-22 17:31:23 +0200

    adding package.json

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,5 +2,10 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Hello World" }),
+      "isBase64Encoded": true|false,
+        "statusCode": httpStatusCode,
+        "headers": { "headerName": "headerValue"},
+        "multiValueHeaders": { "headerName": ["headerValue", "headerValue2",],},
+        "body": "..."
     };
   }
