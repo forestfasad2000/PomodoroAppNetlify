109701dfee3446b6a5625570ae5ec9b42c86cc34
forestfasad2000 <forestfasad@gmail.com>
2022-02-23 18:00:01 +0200

    background

--- dist/netlify/functions/hello.js
+++ dist/netlify/functions/hello.js
@@ -2,10 +2,5 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Hello World" }),
-      "isBase64Encoded": true|false,
-        "statusCode": httpStatusCode,
-        "headers": { "headerName": "headerValue"},
-        "multiValueHeaders": { "headerName": ["headerValue", "headerValue2",],},
-        "body": "..."
     };
   }
